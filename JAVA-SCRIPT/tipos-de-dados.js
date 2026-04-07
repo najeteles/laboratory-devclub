d9847d641fe72754ad6ee7955f08313e02d404c8
najeteles <najefreud@gmail.com>
2026-04-07 10:26:00 -0300

    criacao-file-tipo-de-dados

---
+++ JAVA-SCRIPT/tipos-de-dados.js
@@ -0,0 +1,27 @@
+/*
+1- String -> Textos
+
+
+*/ 
+const texto1 = "textos com aspas 'duplas'"
+const texto2 = 'textos com aspas "simples"'
+const texto3 = `textos
+ com 
+ a 
+ crase` // Template literals ou Template Strings
+ 
+ console.log(texto1)
+ console.log(texto2)
+ console.log(texto3)
+
+
+const myAge = 46
+const myCar = 'C3'
+const myHome = 'no campo'
+
+const mystring = `MInha idade é ${myAge} , meia idade é foda.
+Quero vender meu ${myCar} e morar ${myHome}.`   
+
+console.log(mystring)
+
+
