e4fd91c79a9ad4bf7a7730e0ed15b1ea3fb9264d
najeteles <najefreud@gmail.com>
2026-04-21 01:48:31 -0300

    Projeto Sorteio

---
+++ JAVA-SCRIPT/Math.js
@@ -0,0 +1,48 @@
+/*
+Math -> Matemática
+ - pow - Potência
+ - sqrt - Raiz quadrada
+ - cbrt - Raiz cúbica
+ - abs - Valor absoluto
+ - ceil - Arredondar para cima
+ - floor - Arredondar para baixo
+ - round - Arredondar para o inteiro mais próximo
+ - max - Maior valor
+ - min - Menor valor
+ - pi - Constante π 3,14159...
+ - random - Número aleatório entre 0 e 1
+ */
+
+const result = Math.pow(2, 3);
+console.log(result); // 8
+
+const sqrtResult = Math.sqrt(16);
+console.log(sqrtResult); // 4
+
+const cbrtResult = Math.cbrt(27);
+console.log(cbrtResult); // 3
+
+const absResult = Math.abs(-5);
+console.log(absResult); // 5
+
+const ceilResult = Math.ceil(4.2);
+console.log(ceilResult); // 5
+
+const floorResult = Math.floor(4.8);
+console.log(floorResult); // 4
+
+const roundResult = Math.round(4.5);
+console.log(roundResult); // 5
+
+const maxResult = Math.max(1, 5, 3);
+console.log(maxResult); // 5
+
+const minResult = Math.min(1, 5, 3);
+console.log(minResult); // 1
+
+const randomResult = Math.random();
+console.log(randomResult); 
+
+const piResult = Math.pi;
+console.log(piResult); // 3.141592653589793
+ 
